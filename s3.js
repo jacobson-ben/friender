86a4050d075efdd994bf15cd4b7b861a21866f42
Kat Huang <khuang129@gmail.com>
2021-04-23 16:42:28 -0700

    changed s3 functions

--- s3.js
+++ s3.js
@@ -1,40 +1,35 @@
-require('dotenv').config()
-const fs = require('fs')
-const S3 = require('aws-sdk/clients/s3')
+require("dotenv").config();
 
-const bucketName = process.env.AWS_BUCKET_NAME
-const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SECRET_KEY
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
 
-const s3 = new S3({
+var aws = require("aws-sdk");
+var multer = require("multer");
+var multerS3 = require("multer-s3");
+
+aws.config.update({
   region,
   accessKeyId,
-  secretAccessKey
-})
+  secretAccessKey,
+});
 
-// uploads a file to s3
-function uploadFile(file) {
-  const fileStream = fs.createReadStream(file.path)
+const s3 = new aws.S3();
 
-  const uploadParams = {
-    Bucket: bucketName,
-    Body: fileStream,
-    Key: file.filename
-  }
+//can add file, uuid
 
-  return s3.upload(uploadParams).promise()
-}
-exports.uploadFile = uploadFile
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: bucketName,
+    acl: "public-read",
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
 
+console.log(process.env);
 
-// downloads a file from s3
-function getFileStream(fileKey) {
-  const downloadParams = {
-    Key: fileKey,
-    Bucket: bucketName
-  }
-
-  return s3.getObject(downloadParams).createReadStream()
-}
-exports.getFileStream = getFileStream
+exports.upload = upload;
